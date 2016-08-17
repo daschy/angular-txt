625d2714d894ebe915ac56174b5e91eef7ee321f
daschy <miloertola@gmail.com>
2016-08-17 14:29:55 +0200

    boilerplate with tests

---
+++ src/angular-txt.js
@@ -0,0 +1,13 @@
+/**
+ * @ngdoc module
+ * @name angular.txt
+ * @description
+ * angular directive for teletext.io
+ */
+
+(function () {
+  angular.module('angular.txt', [])
+    .directive('ngDataTxt', function () {
+
+    });
+})();
