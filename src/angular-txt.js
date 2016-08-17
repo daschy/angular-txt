edf8b4c983830b9e0fb3bc9d4b41d2c23dbb1f59
daschy <miloertola@gmail.com>
2016-08-17 16:58:21 +0200

    added specs

--- src/angular-txt.js
+++ src/angular-txt.js
@@ -7,7 +7,25 @@
 
 (function () {
   'use strict';
-  var app = angular.module('angular.txt', [])
+
+  function requireDep() {
+    try {
+      return require('txt.js'); // Using nw.js or browserify?
+    } catch (e) {
+      throw new Error('Please install txt.js via npm. Please reference to: https://github.com/daschy/angular-txt'); // Add wiki/troubleshooting section?
+    }
+  }
+
+  angular.module('angular.txt', [])
+    .config(function () {
+      if (typeof Txt === 'undefined') {
+        if (typeof require === 'function') {
+          Txt = requireDep();
+        } else {
+          throw new Error('txt.js cannot be found by angular-txt! Please reference to: https://github.com/daschy/angular-txt'); // Add wiki/troubleshooting section?
+        }
+      }
+    })
     .provider('dataTxt', function () {
       this.$get = ['$window', '$rootScope', function ($window, $rootScope) {}];
 
--- test/spec/angular-txt.spec.js
+++ test/spec/angular-txt.spec.js
@@ -2,10 +2,22 @@
 
 describe('angularTxt', function () {
 
+  var $compile;
+  var $scope;
+
   beforeEach(module('angular.txt'));
 
-  it('first test', function () {
+  beforeEach(inject(function (_$compile_, _$rootScope_) {
+    $compile = _$compile_;
+    $scope = _$rootScope_;
+  }));
 
-  });
+  it('dataTxt provider', inject(function () {
+    expect(true).to.be(false);
+  }));
+
+  it('ng-data-txt directive', inject(function () {
+    // expect(info.getUserInfo()).to.be.not.null;
+  }));
 
 });
